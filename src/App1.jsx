2b5b4b12f84ae3d43fd92030e76a76883889e088
Krish-006-19 <krish.ramena24@vit.edu>
2025-04-09 00:22:46 +0530

    Initial Commit

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom'
 import App from './App'
 import Register from './features/Register'
 import Login from './features/Login'
@@ -9,6 +9,7 @@ function App1() {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to="/MyLinkedin" />} />  
           <Route path='/MyLinkedin' element={<App />} />
           <Route path='/MyLinkedin/signin' element={<Login/>}/>
           <Route path='/MyLinkedin/register' element={<Register />} />
